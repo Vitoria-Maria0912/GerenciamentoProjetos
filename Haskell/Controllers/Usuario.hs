198a0aff3b575d15fbc092788d4eb5643c3967a3
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2023-09-05 15:00:49 -0300

    Update da geracao do id e da verificacao do id

--- Haskell/Controllers/Usuario.hs
+++ Haskell/Controllers/Usuario.hs
@@ -21,5 +21,12 @@ removeUsuario = deletaUsuarioDatabase -- executada em Database
 
 
 -- gera o número de Id do usuário de acordo com seu local na lista 
-geraIdUsuario :: [Usuario] -> IO()
+geraIdUsuario :: [Usuario] -> Int
 geraIdUsuario usuarios = length usuarios
+
+
+
+-- verifica se o Id existe na lista de usuarios (usando o valor de idUsuarios linkado ao Usuario)
+-- Caso o valor do Id ja exista e ele for menor que o tamanho da lista (caaso onde houve remoção de usuario), adicionar mais um quando gerar o ID
+verificaIdUsuario :: Int -> [Usuario] -> Bool
+verificaIdUsuario id usuarios = elem id (map Usuario.idUsuario usuarios)
