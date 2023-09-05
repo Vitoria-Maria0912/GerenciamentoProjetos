543904a3739593660b425f680154e5a7cd0e6652
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2023-09-05 17:38:15 -0300

    update em cria usuario, remove usuario e remocao do geraID

--- Haskell/Controllers/Usuario.hs
+++ Haskell/Controllers/Usuario.hs
@@ -11,22 +11,15 @@ data Usuario = Usuario {
 }
 
 -- criação de usuário
-criaUsuario :: String -> String -> IO()
+criaUsuario :: Int -> String -> String -> IO()
 criaUsuario = criaUsuarioDatabase-- executada em Database
 
 
 -- remoção de usuário
-removeUsuario :: String -> IO()
+removeUsuario :: Int -> IO()
 removeUsuario = deletaUsuarioDatabase -- executada em Database
 
 
--- gera o número de Id do usuário de acordo com seu local na lista 
-geraIdUsuario :: [Usuario] -> Int
-geraIdUsuario usuarios = length usuarios
-
-
-
 -- verifica se o Id existe na lista de usuarios (usando o valor de idUsuarios linkado ao Usuario)
--- Caso o valor do Id ja exista e ele for menor que o tamanho da lista (caaso onde houve remoção de usuario), adicionar mais um quando gerar o ID
 verificaIdUsuario :: Int -> [Usuario] -> Bool
 verificaIdUsuario id usuarios = elem id (map Usuario.idUsuario usuarios)
