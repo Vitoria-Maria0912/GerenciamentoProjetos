4c535f6995fcfba8e892b866232907d02e928f73
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2023-09-05 21:05:58 -0300

    Removendo getUsuario, verificaiduser e formatauser (foram p util)

--- Haskell/Controllers/Usuario.hs
+++ Haskell/Controllers/Usuario.hs
@@ -17,22 +17,3 @@ criaUsuario = criaUsuarioDatabase-- executada em Database
 -- remoção de usuário
 removeUsuario :: Int -> IO()
 removeUsuario = deletaUsuarioDatabase -- executada em Database
-
--- verifica se o Id existe na lista de usuarios (usando o valor de idUsuarios linkado ao Usuario)  (ver se precisa colocar na Database)
-verificaIdUsuario :: Int -> [Usuario] -> Bool
-verificaIdUsuario id usuarios = elem id (map Usuario.idUsuario usuarios)
-
--- retorna a representacao do usuario em string (ver se precisa colocar na Database)
-getUsuario:: Int -> [Usuario] -> Maybe String
-getUsuario id usuarios =
-  case filter (\u -> idUsuario u == id) usuarios of [usuarioEncontrado] -> Just (formataUsuario usuarioEncontrado) 
-  _ -> Nothing
-
--- formata em string dados de um usuario  (ver se precisa colocar na Database)
-formataUsuario:: Usuario -> String
-formataUsuario usuario = 
-   "ID: " show (idUsuario usuario) ++ "\n" ++
-   "Nome: " nome usuario ++ "\n"
-
-
-
