0a670617eba0dbfae1a462e79cd29d38d56f605a
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2023-09-08 19:27:40 -0300

    atualizando usuario pra voltar ao txt mais simples (sem database)

--- Haskell/Controllers/Usuario.hs
+++ Haskell/Controllers/Usuario.hs
@@ -10,11 +10,36 @@ data Usuario = Usuario {
     senha :: String
 }
 
--- Criação de um Usuário
-criaUsuario :: String -> String -> String -> IO()
-criaUsuario = criaUsuarioDatabase-- executada em Database
+-- cria um usuario
+criaUsuario :: String -> String -> String -> Usuario
+criasuario idUsuario nome senha = 
+  (Usuario {idUsuario = idUsuario, nome = nome, senha = senha})
 
-
--- Remoção de um Usuário, pelo ID
-removeUsuario :: String -> IO()
-removeUsuario = deletaUsuarioDatabase -- executada em Database
+-- checa se existe um usuario com esse id, e se nao existe, adiciona na lista de usuários
+adicionarUsuario :: Usuario -> [Usuario] -> [Usuario]
+adicionarUsuario usuario usuarios =
+    case find (\u -> idUsuario u == idUsuario usuario) usuarios of
+        Just _ -> usuarios
+        Nothing -> usuario : usuarios
+        
+-- escreve informações sobre usuários em um arquivo
+escreverUsuarios :: FilePath -> [Usuario] -> IO ()
+escreverUsuarios arquivo usuarios = appendFile arquivo conteudo
+  where
+    conteudo = unlines $ map formatarUsuario usuarios
+    formatarUsuario u = "ID: " ++ show (idUsuario u) ++ ", NOME: " ++ nome u ++ ", FUNÇÃO: " ++ funcao u
+    
+-- ler informações sobre usuários de um arquivo -> retorna uma lista de usuários
+lerUsuarios :: FilePath -> IO [Usuario.Usuario]
+lerUsuarios path = do
+  conteudo <- readFile path
+  let usuarios = mapMaybe Usuario.fromString $ lines conteudo
+  return usuarios
+  
+-- converte uma string em um objeto do tipo Usuario
+fromString :: String -> Maybe Usuario
+fromString str = case words str of
+  [idStr, nome, funcao] -> do
+    idUsuario <- readMaybe idStr
+    return Usuario{idUsuario = idUsuario, nome = nome, funcao = funcao}
+  _ -> Nothing
