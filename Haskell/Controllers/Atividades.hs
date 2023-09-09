5f32efa38f6c02ccb09ba099e37dbcd968d9bda5
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2023-09-09 17:29:43 -0300

    Update Atividades.hs

--- Haskell/Controllers/Atividades.hs
+++ Haskell/Controllers/Atividades.hs
@@ -4,7 +4,6 @@ import Data.List (find)
 import Controllers.Usuario
 import Database.Database
 
-
 data Atividade = Atividade {
     titulo :: String,
     descricao :: String,
@@ -17,17 +16,6 @@ data Atividade = Atividade {
 
 criarAtividade :: String -> String -> String -> String -> String -> Maybe String -> Maybe [String] -> IO()
 criarAtividade = criaAtividadeDatabase
-
--- usar esse depois e excluir o de cima:
--- criarAtividade :: String -> String -> String -> String -> String -> Maybe String -> Maybe [String] -> Atividade
--- criarAtividade titulo descricao status idProjetoAtividade idAtividade idMembroResponsavel feedback =
---    Atividade { titulo = titulo, 
---                descricao = descricao, 
---                idProjetoAtividade = idProjetoAtividade,
---                idAtividade = idAtividade,
---                status = status, 
---                idMembroResponsavel = idMembroResponsavel,
---                feedback = feedback }
 
 -- Muda o status de uma atividade
 mudaStatus :: Atividade -> String -> Atividade
@@ -48,58 +36,36 @@ adicionaFeedback atividade comentario = do
     case maybeFeedback of
         Just feedbacksAtuais -> Just (comentario : feedbacksAtuais)
         Nothing -> Just [comentario]
-    
--- Adiciona uma atividade no sistema (com base no id - nao compila ainda)
--- adicionaAtividade :: Atividade -> [Atividade] -> [Atividade]
--- adicionaAtividade atividade atividades = 
---    case find (\u -> idAtividade u == idAtividade atividade) atividades of 
---        Just _-> atividades
---        Nothing -> atividade : atividades
-        
--- escreve atividade no txt
--- escreverAtividades :: FilePath -> [Atividade] -> IO ()
--- escreverAtividades arquivo atividades = appendFile arquivo conteudo
---  where
---    conteudo = unlines $ map formatarAtividade atividades
---    formatarAtividade a = "ID: " ++ idAtividade a ++ ", TITULO: " ++ titulo a ++ ", DESCRIÇÃO: " ++ descricao a ++ ", ID PROJETO: " ++ idProjetoAtividade a ++ ", STATUS: " ++ status a ++ ", MEMBRO RESPONSÁVEL: " ++ membroResponsavel a
 
+
+-- YALLE FEZ:
+
+
+
+-- (NÃO COMPILA)
 -- le as atividades do txt e retorna a lista
 -- lerAtividades :: FilePath -> IO [Atividade]
 -- lerAtividades path = do
---  conteudo <- readFile path
---  let atividades = mapMaybe fromStringAtv $ lines conteudo
---  return atividades
+--     conteudo <- readFile path
+--     let atividades = mapMaybe fromStringAtv $ lines conteudo
+--     return atividades
   
+-- (NÃO COMPILA)
 -- converte uma string em um objeto do tipo Atividade
 -- fromStringAtv :: String -> Maybe Atividade
 -- fromStringAtv str = case words str of
---  [titulo, descricao, status, idProjetoAtividade, idAtividade, idMembroResponsavel, feedback] -> do
---    return Atividade {
---      titulo = titulo,
---      descricao = descricao,
---      status = status,
---      idProjetoAtividade = idProjetoAtividade,
---      idAtividade = idAtividade,
---      idMembroResponsavel = idMembroResponsavel,
---      feedback = feedback
---    }
---  _ -> Nothing
+--   [titulo, descricao, status, idProjetoAtividade, idAtividade, idMembroResponsavel, feedback] -> do
+--     return Atividade {titulo = titulo,
+--                       descricao = descricao, 
+--                       status = status, 
+--                       idProjetoAtividade = idProjetoAtividade, 
+--                       idAtividade = idAtividade, 
+--                       idMembroResponsavel = idMembroResponsavel, 
+--                       feedback = feedback}
+--   _ -> Nothing
 
 
 
--- Formata a atividade em uma lista com todos os seus atributos
--- formataAtividade:: Atividade -> [String]
--- formataAtividade atividade = 
---    ["Titulo: " ++ titulo atividade ++ "\n" ++
---    "Descrição: " ++ descricao atividade ++ "\n" ++
---    "ID Projeto: " ++ idProjetoAtividade atividade ++ "\n" ++
---    "ID Atividade: " ++ idAtividade atividade ++ "\n" ++
---    -- "ID Membro Responsável: " ++ (getMembroResponsavel (atividade)) ++ "\n" ++
---    "Status: " ++ status atividade ++ "\n"]
-    
 
--- Remove uma atividade do sistema (comentado pra nao usar a database)
--- removerAtividade :: String -> IO()
--- removerAtividade = removeAtividadeDatabase
 
 
