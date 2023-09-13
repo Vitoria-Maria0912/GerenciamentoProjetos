db559b56eba83295397f94a352d8d3805c99b97e
yallecarvalho <54670687+yallecarvalho@users.noreply.github.com>
2023-09-13 16:38:34 -0300

    atualiza nome de getUsuario e getUsuarios  e add imprimi usuario

--- Haskell/Controllers/Usuario.hs
+++ Haskell/Controllers/Usuario.hs
@@ -21,68 +21,70 @@ data Usuario = Usuario {
 instance FromJSON Usuario
 instance ToJSON Usuario
 
-{-
-instance Show Usuario where
-  show :: Usuario -> String
-  show (Usuario idUsuario nome _) =  "Olá Usuario:\n" ++
-                                       "Seu idUsuario: " ++ show idUsuario ++ "\n" ++
-                                       "Nome: " ++ nome
--}
 
--- retorna o usuario de acordo com o ID
-getUsarioPorID :: Int-> [Usuario] -> Usuario
-getUsarioPorID _ [] = Usuario (-1) "" ""
-getUsarioPorID idUsuarioS (x:xs)     
+-- | Função que retorna os dados de um usuário de acordo com o seu ID
+getUsario:: Int-> [Usuario] -> Usuario
+getUsario _ [] = Usuario (-1) "" ""
+getUsario idUsuarioS (x:xs)     
   | (idUsuario x) == idUsuarioS = x
   | otherwise = getUsarioPorID idUsuarioS xs
 
--- retorna a lista de usuários lida do arquivo
-getUsuario :: String -> [Usuario]
-getUsuario path = do
+-- | Função que retorna a lista atual de usuários cadastrados no sistema lendo o arquivo.
+getUsuarios :: String -> [Usuario]
+getUsuarios path = do
  let file = unsafePerformIO( B.readFile path )
  let decodedFile = decode file :: Maybe [Usuario]
  case decodedFile of
   Nothing -> []
-  Just out -> out
+  Just usuarios -> usuarios
 
--- cria e salva o usuario no arquivo
+-- | Função que imprime o usuário omitindo informação sensível
+imprimirUsuario :: Usuario -> IO()
+imprimirUsuario u = putStrLn $ "ID: " ++ show (idUsuario u) ++ ", Nome: " ++ nome u
+
+-- | Função que salva e escreve o usuario no arquivo diretamente
 salvarUsuario :: String -> Int -> String -> String -> IO()
 salvarUsuario jsonFilePath idUsuario nome senha = do
- let novoId = (length (getUsuario jsonFilePath)) + 1
+ let novoId = (length (getUsuarios jsonFilePath)) + 1
  let u = Usuario novoId nome senha
- let userList = (getUsuario jsonFilePath) ++ [u]
+ let userList = (getUsuarios jsonFilePath) ++ [u]
 
  B.writeFile "../Temp.json" $ encode userList
  removeFile jsonFilePath
  renameFile "../Temp.json" jsonFilePath
 
- -- remove usuario pelo ID
+-- | Função que remove usuario pelo ID
 removeUsarioPorID :: Int -> [Usuario] -> [Usuario]
 removeUsarioPorID _ [] = []
 removeUsarioPorID idUsuarioS(x:xs)
  | (idUsuario x) == idUsuarioS = xs
  | otherwise = [x] ++ (removeUsarioPorID idUsuarioS xs)
 
--- remove o usuario do arquivo
+-- | Função que remove usuario da lista de usuarios reescrevendo o arquivo.
 removerUsuario :: String -> Int -> IO()
 removerUsuario jsonFilePath idUsuario = do
- let usuarios = getUsuario jsonFilePath
+ let usuarios = getUsuarios jsonFilePath
  let novosUsuarios = removeUsarioPorID idUsuario usuarios
 
  B.writeFile "../Temp.json" $ encode novosUsuarios
  removeFile jsonFilePath
  renameFile "../Temp.json" jsonFilePath
 
--- retorna o numero de usuarios cadastrados até o momento (apagar depois de refazer o menu)
+-- | Função que retorna o numero de usuarios atuais do sistema (de acordo com o arquivo)
 getNumDeUsuarios :: String -> Int
-getNumDeUsuarios jsonFilePath = length (getUsuario jsonFilePath)
+getNumDeUsuarios jsonFilePath = length (getUsuarios jsonFilePath)
 
 
 
 main :: IO()
 main = do
-    salvarUsuario "./dados.json" 1000 "Iris" "Iago"
-    putStrLn (show (getUsuario "./dados.json"))
-    removerUsuario "./dados.json" 1
-    putStrLn (show (getUsuario "./dados.json"))
-    putStrLn (show (getUsarioPorID 6 (getUsuario "./dados.json")))
+    --salvarUsuario "./dados.json" 1000 "Iris" "Iago"
+    --salvarUsuario "./dados.json" 1750 "Yalle" "tuts"
+    --putStrLn (show (getUsuario "./dados.json"))
+   -- removerUsuario "./dados.json" 1
+  let usuarios = getUsuarios "./dados.json"
+  mapM_ imprimirUsuario usuarios 
+    -- putStrLn (show (getUsarioPorID 2 (getUsuario "./dados.json")))
+   -- salvarUsuario "./dados.json" 10 "Vitoria" "ruiva"
+   -- putStrLn (show (getUsuario "./dados.json"))
+   -- putStrLn (show (getNumDeUsuarios "./dados.json"))
