c1d7b6f97f58da04a3585bccc2d58bd61c1de096
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2023-09-26 20:52:55 -0300

    Traduzindo para Prolog

--- Prolog/Menus/HelloWorld.pl
+++
@@ -1,7 +0,0 @@
-% Este é um programa "Hello, World!" em Prolog.
-hello_world :-
-    write('Hello, World!'),
-    nl. % Inserir uma quebra de linha.
-
-% Chamando o predicado hello_world para executar o programa.
-:- hello_world.
