2f8e4541c3fb54e237d4e1be014c673f8a84ff3c
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2023-10-13 16:32:12 -0300

    Material dos monitores

---
+++ Prolog/Casos De Uso/casosDeUso.pl
@@ -0,0 +1,17 @@
+% * SALVAR ATIVIDADES DO BANCO
+% salvarAtividade("bancoDeAtividades.json", Titulo, Descricao, Status, Dificuldade, Id_Projeto_Atividade, Id_Atividade, Id_Membro_Responsavel).
+
+% salvarAtividade("Database/bancoDeAtividades.json", "Desenvolvimento iOS", "Desenvolver um aplicativo mobile para iOS", "Difícil", 16160).
+% salvarAtividade("Database/bancoDeAtividades.json", "Configuração do Servidor", Configurar um servidor web usando Apache, "Não atribuída!", "Média", null, 16160, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Algoritmo de Ordenação", "Criar um algoritmo de ordenação eficiente", "Não atribuída!", "Fácil", null, 10970, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Testes de Segurança", "Realizar testes de segurança em um sistema", "Não atribuída!", "Difícil", null, 22062, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Desenvolvimento Web", "Criar um site responsivo usando HTML e CSS", "Não atribuída!", "Média", null, 22755, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Otimização de Banco de Dados", "Otimizar o desempenho de um banco de dados SQL", "Não atribuída!", "Fácil", null, 55856, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Desenvolvimento de Jogos", "Desenvolver um jogo em Unity", "Não atribuída!", "Difícil", null, 63841, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Configuração de Firewall", "Configurar um firewall para proteção de rede", "Não atribuída!", "Média", null, 60185, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Desenvolvimento de Chatbot", "Programar um chatbot com IA", "Não atribuída!", "Fácil", null, 67609, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Desenvolvimento iOS", "Criar um programa de análise de dados em Python", "Não atribuída!", "Difícil", null, 98002, null).
+% salvarAtividade("Database/bancoDeAtividades.json", "Análise de Dados em Python", "Desenvolver um aplicativo mobile para iOS", "Não atribuída!", "Média", null, 41022, null).
+
+% * REMOVER ATIVIDADES DO BANCO
+% re
